4ca7e701ddedb80a04833dff0a1b323619cdf632
Shlomi Unger <shlomi.unger@gmail.com>
2024-05-16 21:57:47 +0300

    added warning in case message is too long for OP_RETURN

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from utf8_converter import utf8_hex_representation
 def main():
     input_text = input("Enter the text: ")
     hex_representation = utf8_hex_representation(input_text)
-    print(hex_representation)
+    print(f"\n{hex_representation}")
+
+    num_bytes = len(hex_representation) // 2
+    if num_bytes > 80:
+        num_bytes_over_limit = num_bytes - 80
+        print(f"\n{num_bytes} bytes - You are {num_bytes_over_limit} bytes over the OP_RETURN limit")
+
 
 
 # If the script is executed directly (not imported as a module), call the main function
